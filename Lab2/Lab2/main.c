5927b1af75e81cccaa6e9298220b95bb1d484fa5
Daniel Björk <ddbjork@gmail.com>
2017-02-09 17:16:13 +0100

    fixed yeild local

--- Lab2/Lab2/main.c
+++ Lab2/Lab2/main.c
@@ -63,9 +63,9 @@ void computePrimes(int pos) {
     for(n = 1; ; n++) {
         if (is_prime(n)) {
             printAt(n, pos);
+            yield();
         }
     }
-    yield();
 }
 
 int main() {
