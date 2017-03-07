0f9830d2b0de20dfd4d48bc19ad0beb8dc1ff10a
Leo Ozolins Carlson <leocar-2@student.ltu.se>
2017-03-07 10:28:29 +0100

    minor update

--- Lab4/Lab4/GUI.h
+++ Lab4/Lab4/GUI.h
@@ -3,7 +3,7 @@
  *
  * Created: 17/02/23 15:42:32
  *  Author: Daniel
- */ 
+ */
 
 #ifndef GUI_H_
 #define GUI_H_
@@ -14,45 +14,49 @@
 
 #define init_GUI(_pwm, _pos) {initObject(), _pwm, _pos, 1, 0, 0}
 typedef struct {
-	Object super;
-	PWM p;
-	int pos;
-	int writeEn;
-	int altFreq;
-	int blinkCount;
+    Object super;
+    PWM p;
+    int pos;
+    int writeEn;
+    int altFreq;
+    int blinkCount;
 } GUI;
 
 int _GUI_print(GUI *g, int arg);
+
 int _GUI_clear(GUI *g, int arg);
+
 int _GUI_blinkLoop(GUI *g, int arg);
+
 int _GUI_setAltFreq(GUI *g, int arg);
+
 int _GUI_getAltFreq(GUI *g, int arg);
 
 #define GUI_init(_GUI) \
-	GUI_fprint(_GUI); \
-	ASYNC(&((_GUI)->p), PWM_init_pulseLoop, 0)
+    GUI_fprint(_GUI); \
+    ASYNC(&((_GUI)->p), PWM_init_pulseLoop, 0)
 
 #define GUI_print(_GUI) \
-	ASYNC(_GUI, _GUI_print, 0)
+    ASYNC(_GUI, _GUI_print, 0)
 
 #define GUI_fprint(_GUI) \
-	ASYNC(_GUI, _GUI_print, 1)
+    ASYNC(_GUI, _GUI_print, 1)
 
 #define GUI_clear(_GUI) \
-	ASYNC(_GUI, _GUI_clear, 0)
+    ASYNC(_GUI, _GUI_clear, 0)
 
 #define GUI_stopBlink(_GUI) \
-	(_GUI)->blinkCount = 0; \
-	GUI_fprint(_GUI)
+    (_GUI)->blinkCount = 0; \
+    GUI_fprint(_GUI)
 
 #define GUI_startBlink(_GUI) \
-	(_GUI)->blinkCount = 0; \
-	ASYNC(_GUI, _GUI_blinkLoop, 0)
+    (_GUI)->blinkCount = 0; \
+    ASYNC(_GUI, _GUI_blinkLoop, 0)
 
 #define GUI_getAltFreq(_GUI) \
-	SYNC(_GUI, _GUI_getAltFreq, 0)
+    SYNC(_GUI, _GUI_getAltFreq, 0)
 
 #define GUI_setAltFreq(_GUI, _set) \
-	ASYNC(_GUI, _GUI_setAltFreq, _set)
+    ASYNC(_GUI, _GUI_setAltFreq, _set)
 
 #endif /* GUI_H_ */
--- Lab41/Lab41/GUI.c
+++ Lab41/Lab41/GUI.c
@@ -3,53 +3,53 @@
  *
  * Created: 17/02/23 15:43:43
  *  Author: Daniel
- */ 
+ */
 
 #include "GUI.h"
 
-int _GUI_print(GUI *g, int arg){
-	if(g->writeEn || arg){
-		printAt(g->super.freq, g->pos);
-		g->writeEn = 1;
-	}
-	return 0;
+int _GUI_print(GUI *g, int arg) {
+    if (g->writeEn || arg) {
+        printAt(g->super.freq, g->pos);
+        g->writeEn = 1;
+    }
+    return 0;
 }
 
-int _GUI_clear(GUI *g, int arg){
-	clearAt(g->pos);
-	g->writeEn = 0;
-	return 0;
+int _GUI_clear(GUI *g, int arg) {
+    clearAt(g->pos);
+    g->writeEn = 0;
+    return 0;
 }
 
-int _GUI_blinkLoop(GUI *g, int arg){
-	if(arg == g->blinkCount){
-		g->blinkCount++;
-		if(arg%2){
-			GUI_clear(g);
-			AFTER(MSEC(300), g, _GUI_blinkLoop, arg+1);
-		}else{
-			GUI_fprint(g);
-			AFTER(MSEC(700), g, _GUI_blinkLoop, arg+1);
-		}
-	}
-	return 0;
+int _GUI_blinkLoop(GUI *g, int arg) {
+    if (arg == g->blinkCount) {
+        g->blinkCount++;
+        if (arg % 2) {
+            GUI_clear(g);
+            AFTER(MSEC(300), g, _GUI_blinkLoop, arg + 1);
+        } else {
+            GUI_fprint(g);
+            AFTER(MSEC(700), g, _GUI_blinkLoop, arg + 1);
+        }
+    }
+    return 0;
 }
 
-int _GUI_swapFreq(GUI *g, int arg){
-	if(g->super.freq == 0){
-		g->super.freq = g->altFreq;
-		g->altFreq = 0;
-	}else{
-		g->altFreq = g->super.freq;
-		g->super.freq = 0;
-	}
+int _GUI_swapFreq(GUI *g, int arg) {
+    if (g->super.freq == 0) {
+        g->super.freq = g->altFreq;
+        g->altFreq = 0;
+    } else {
+        g->altFreq = g->super.freq;
+        g->super.freq = 0;
+    }
 }
 
-int _GUI_getAltFreq(GUI *g, int arg){
-	return g->altFreq;
+int _GUI_getAltFreq(GUI *g, int arg) {
+    return g->altFreq;
 }
 
-int _GUI_setAltFreq(GUI *g, int arg){
-	g->altFreq = arg;
-	return 0;
+int _GUI_setAltFreq(GUI *g, int arg) {
+    g->altFreq = arg;
+    return 0;
 }
