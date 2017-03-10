b4557d03c7ddb3e4ae3f12cb76465f59f8afae49
Leo Ozolins Carlson <leocar-2@student.ltu.se>
2017-03-10 11:43:37 +0100

    no clue

--- Lab5/Lab5/CONTROLLER.c
+++ Lab5/Lab5/CONTROLLER.c
@@ -2,3 +2,15 @@
 #include "TinyTimber.h"
 #include "CONTROLLER.h"
 
+int NBOB;
+int SBOB;
+
+void bridge_clear(void) {
+ /*check if NBOB and SBOB is false*/
+if (NBOB && SBOB){
+/*return that bridge is clear and set both red lights to true*/
+}
+
+ /*if neither is true, the bridge is clear*/
+ /*set both lights to red (?)*/
+}
