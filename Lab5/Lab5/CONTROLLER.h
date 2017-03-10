eea65703f678e58a90e839b8bf466f3968a71cf5
Leo Ozolins Carlson <leocar-2@student.ltu.se>
2017-03-10 09:22:47 +0100

    Controller.C

--- Lab5/Lab5/CONTROLLER.h
+++ Lab5/Lab5/CONTROLLER.h
@@ -1,19 +1,10 @@
-/*
- * CONTROLLER.h
- *
- * Created: 2017-03-09 16:55:01
- *  Author: Leo
- */ 
-
-
-
 #ifndef CONTROLLER_H_
 #define CONTROLLER_H_
 
 #include <avr/io.h>
 #include "TinyTimber.h"
 
-/*Sesor bits*/
+/*Sensor bits*/
 #define NBCASA 0	/*Northbound car arrival sensor activated*/
 #define NBBESA 1	/*Northbound bridge entry sensor activated*/
 #define SBCASA 2	/*Southbound car arrival sensor activated*/
@@ -24,6 +15,11 @@
 #define NBRLS 1		/*Northbound red light status*/
 #define SBGLS 3		/*Southbound green light status*/
 #define SBRLS 4		/*Southbound red light status*/
+
+typedef struct {
+	int NBOB;	/*Northbound on bridge*/
+	int SBOB;	/*Southbound on bridge*/
+};
 
 
 #endif /* CONTROLLER_H_ */
---
+++ Lab5/Lab5/USART.h
@@ -0,0 +1,16 @@
+/*
+ * USART.h
+ *
+ * Created: 2017-03-09 17:17:52
+ *  Author: Leo
+ */ 
+
+
+#ifndef USART_H_
+#define USART_H_
+
+
+
+
+
+#endif /* USART_H_ */
